9af3c7d61c5d066cb4e779d968855ea8163ce3c2
Danil_Dimitrov <danil6608@mail.ru>
2025-03-10 19:33:17 +0300

    Создание моделей

---
+++ src/main/java/ru/project/subtrack/models/Subscription.java
@@ -0,0 +1,51 @@
+package ru.project.subtrack.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "subscriptions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String serviceName; // Например, "Яндекс Плюс", "VK Музыка"
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; // Дата создания
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt; // Дата последнего обновления
+
+    // Метод для автоматического установления времени при создании подписки
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+    }
+
+    // Метод для обновления времени при изменении подписки
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now(); // Обновляем только updatedAt при изменении
+    }
+}
